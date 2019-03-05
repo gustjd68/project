6995a6ddaa46e635fb5039add0d8f354e8fb04ea
gustjd68 <38155910+gustjd68@users.noreply.github.com>
2019-03-06 08:04:18 +0900

    F

--- blogproject/settings.py
+++ blogproject/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blog.apps.BlogConfig',
+    'potofolio.apps.PotofolioConfig',
 ]
 
 MIDDLEWARE = [
@@ -119,3 +120,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'potofolio', 'static')
+]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
